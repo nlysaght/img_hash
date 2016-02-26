d13f4c977d50453ac963faace0c6d32568868581
noel-honksw-com <noel@honksw.com>
2016-02-26 16:16:33 +0000

    Added in some download code for the images.

--- img_hash2/img_hash2/Program.cs
+++ img_hash2/img_hash2/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,32 @@ namespace img_hash2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void  Main(string[] args)
         {
+            var output_folder = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "downloaded");
+            var BASE_URL = "http://www.credit-card-logos.com/";
+            var client = new System.Net.Http.HttpClient();
+            var html = client.GetAsync(BASE_URL).GetAwaiter().GetResult().Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//img"))
+            {
+                string src = link.GetAttributeValue("src",string.Empty);
+                if (src.StartsWith("http://", StringComparison.InvariantCultureIgnoreCase))
+                    continue;
+                var img_url = BASE_URL + src;
+                var img_bytes = client.GetAsync(img_url).GetAwaiter().GetResult().Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+                // save locally
+                var file_name = img_url.Substring(img_url.LastIndexOf("/") + 1);
+                var full_name = Path.Combine(output_folder, file_name);
+                if(File.Exists(full_name))
+                {
+                    file_name = Path.GetFileNameWithoutExtension(file_name) + "_" + System.Environment.TickCount.ToString() + Path.GetExtension(file_name);
+                }
+                Console.WriteLine($"Writing file {img_url} {file_name}");
+                Directory.CreateDirectory(output_folder);
+                File.WriteAllBytes(Path.Combine(output_folder, file_name), img_bytes);
+            }
         }
     }
 }
